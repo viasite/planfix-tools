a62be9205959284ccd44be657cc1497b336b5c13
terminal <root@popstas-server.popstas.ru>
2021-03-05 17:11:54 +0500

    feat: contacts-update: Переделан отчет по поступлениям: за 30/100/200/365 дней

--- src/commands/contacts_update.js
+++ src/commands/contacts_update.js
@@ -5,7 +5,10 @@ const fs = require('fs');
 
 // id полей Планфикса, из шаблона компании - https://tagilcity.planfix.ru/contact/4504
 const customFields = {
-  value: 105, // Поступления за последние 200 дней
+  value200: 105, // Поступления за 200 дней
+  value30: 133, // Поступления за 30 дней
+  value100: 135, // Поступления за 100 дней
+  value365: 137, // Поступления за 365 дней
   position: 107, // Рейтинг
   totalPercent: 109, // Сколько процентов от общего приносит
 };
@@ -21,8 +24,17 @@ const fieldsMap = {
   contactId: {
     name: 'Номер',
   },
-  value: {
-    name: 'Выручка',
+  value200: {
+    name: 'Выручка200',
+  },
+  value30: {
+    name: 'Выручка30',
+  },
+  value100: {
+    name: 'Выручка100',
+  },
+  value365: {
+    name: 'Выручка365',
   },
 };
 
@@ -36,14 +48,22 @@ async function parseContacts(rows) {
     const contact = {
       id: parseInt(row[fieldsMap.contactId.num]),
       name: row[fieldsMap.contactName.num].trim(),
-      value: row[fieldsMap.value.num]
+      value200: row[fieldsMap.value200.num],
+      value30: row[fieldsMap.value30.num],
+      value100: row[fieldsMap.value100.num],
+      value365: row[fieldsMap.value365.num],
     }
-    contact.value = parseInt(contact.value.replace(/ /g, ''));
-    if (!contact.id || !contact.value) continue;
+    if (contact.value200) contact.value200 = parseInt(contact.value200.replace(/ /g, ''));
+    if (contact.value30) contact.value30 = parseInt(contact.value30.replace(/ /g, ''));
+    if (contact.value100) contact.value100 = parseInt(contact.value100.replace(/ /g, ''));
+    if (contact.value365) contact.value365 = parseInt(contact.value365.replace(/ /g, ''));
+    if (!contact.id || !(contact.value200 || contact.value30 || contact.value100 || contact.value365)) continue;
 
     // получение поля Сайт из контакта
     const pfContact = await api.request('contact.get', {
-      contact: { general: contact.id },
+      contact: {
+        general: contact.id
+      },
     });
     console.log(contact.id);
     console.log(pfContact.contact.name);
@@ -52,10 +72,13 @@ async function parseContacts(rows) {
       contact.site = siteField.value;
     }
 
-    let item = {...contact, ...{
-      position: position,
-      totalPercent: 0,
-    }};
+    let item = {
+      ...contact,
+      ...{
+        position: position,
+        totalPercent: 0,
+      }
+    };
     items.push(item);
 
     position++;
@@ -65,17 +88,18 @@ async function parseContacts(rows) {
   const total = items.length;
   let totalValue = 0;
   items.map((item) => {
-    totalValue += item.value;
+    totalValue += item.value365;
   });
-
   // считаем position and totalPercent
-  items = items.map((item) => {
-    item.position = `${item.position} / ${total}`;
-    item.totalPercent = Math.round((item.value / totalValue) * 100);
-    return item;
-  });
-
+  if (totalValue) {
+    items = items.map((item) => {
+      item.position = `${item.position} / ${total}`;
+      item.totalPercent = Math.round((item.value365 / totalValue) * 100);
+      return item;
+    });
+  }
   // console.log('items: ', items);
+
   return items;
 }
 
@@ -83,6 +107,7 @@ async function parseContacts(rows) {
 async function updateContactFields(contact) {
   const requestFields = {};
   for (let name in customFields) {
+    if (contact[name] == null || contact[name] == undefined) continue;
     requestFields[name] = {
       id: customFields[name],
       value: contact[name],
@@ -95,7 +120,7 @@ async function updateContactFields(contact) {
       customData: requestFields,
     },
   });
-  console.log(`${api.getContactUrl(contact.id)}: `, contact);
+  // console.log(`${api.getContactUrl(contact.id)}: `, contact);
 }
 
 // главная функция
@@ -104,7 +129,9 @@ module.exports = async (opts) => {
 
   const raw = fs.readFileSync(opts.csv, 'utf-8'); // передаётся через ком. строку: --csv data/contragents-values.csv
   const lines = raw.split('\r\n').slice(csvSkipLines); // убирает шапку
-  csvParse(lines.join('\r\n'), { delimiter: ';' }, async (err, rows) => {
+  csvParse(lines.join('\r\n'), {
+    delimiter: ';'
+  }, async (err, rows) => {
     // console.log('rows: ', rows);
     if (err) {
       console.error('CSV не распозналась:');
@@ -115,24 +142,29 @@ module.exports = async (opts) => {
     // ищем номера колонок
     const h = rows[0];
     fieldsMap.contactId.num = h.findIndex((f) => f == fieldsMap.contactId.name);
-    fieldsMap.value.num = h.findIndex((f) => f == fieldsMap.value.name);
+    fieldsMap.value200.num = h.findIndex((f) => f == fieldsMap.value200.name);
+    fieldsMap.value30.num = h.findIndex((f) => f == fieldsMap.value30.name);
+    fieldsMap.value100.num = h.findIndex((f) => f == fieldsMap.value100.name);
+    fieldsMap.value365.num = h.findIndex((f) => f == fieldsMap.value365.name);
 
     // не найдены обязательные поля из fieldsMap
-    if (!fieldsMap.contactId.num || !fieldsMap.value.num) {
+    if (!fieldsMap.contactId.num || !(fieldsMap.value200.num || fieldsMap.value30.num || fieldsMap.value100.num || fieldsMap.value365.num)) {
+      // if (!fieldsMap.contactId.num) {
       console.error(
-        `Таблица не содержит поля: ${fieldsMap.contactId.name}, ${fieldsMap.value.name}`
+        `Таблица не содержит поля: ${fieldsMap.contactId.name}, ${fieldsMap.value200.name}`
       );
       return;
     }
 
     const contacts = await parseContacts(rows);
+    
+        // файл нужен для таблицы сайтов, чтобы в нём выводились данные соответствующих компаний
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    // файл нужен для таблицы сайтов, чтобы в нём выводились данные соответствующих компаний
-    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-
-    // send to planfix
-    for (let contact of contacts) {
-      await updateContactFields(contact);
-    }
+        // send to planfix
+        for (let contact of contacts) {
+          await updateContactFields(contact);
+        }
+        
   });
 };
